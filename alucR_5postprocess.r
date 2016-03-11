bd5d3ca1ca0226a06e54fea769e59b536291cbe6
fg-code <florian.gollnow@geo.hu-berlin.de>
2016-03-11 09:58:41 +0100

    if process in mem

--- alucR_5postprocess.r
+++ alucR_5postprocess.r
@@ -45,6 +45,7 @@ alucR_postprocess <- function (alloc = allocation [[1]],lc , spatial, var.list, 
 	pb <- pbCreate(nsteps= bs$n, ...)
 
 	# chunk processing	
+	if (todisk) {
 	for (i in 1:bs$n) {
 	#read chunks
 	v.lc <- getValues(lc, row=bs$row[i], nrows=bs$nrows[i] )
@@ -72,7 +73,7 @@ alucR_postprocess <- function (alloc = allocation [[1]],lc , spatial, var.list, 
 	v.alloc [ind] <- pseudo.N # natural vegetation to pseudo.N class (including areas of spatial restrictions)
 	
 	# Reclassify pseudo natural class based on trajectory and succession order
-	pseudo.index <- is.element(v.alloc, pseudo.N)
+	pseudo.index <- which(is.element(v.alloc, pseudo.N))
 		
 	if (length (pseudo.index) > 0){  
 		if(length (natural) > 1){
@@ -106,22 +107,76 @@ alucR_postprocess <- function (alloc = allocation [[1]],lc , spatial, var.list, 
 	
 	#### save to raster
 	# write to raster
-	if (todisk) {
 	out <- writeValues(out, v, bs$row[i])
-	} else {
-	cols <- bs$row[i]:(bs$row[i]+bs$nrows[i]-1)
-	for (k in 1:dim(out)[3]){
-	vv.t <- t(matrix( v[, k], nrow=dim(out)[2]))
-	vv [cols,, k] <- vv.t
-	}
-	}
 	pbStep(pb, i)
 	}
-	if (todisk) {
 	out <- writeStop(out)
-	} else {
-	out <- setValues(out, vv)
+	} else { # if(todisk==FALSE)
+	
+	
+	v.lc <- getValues(lc)
+	v.alloc <- getValues(alloc)
+	v.init.years <- getValues(init.years)
+	if(length (spatial) > 0 ){  
+	v.spatial <- getValues(spatial)
 	}
+										
+	#process
+	#no.change
+	if (length(nochange) > 0 ){
+	nochange_index <- is.element(v.lc, nochange)  
+	v.alloc[nochange_index] <- v.lc [nochange_index]
+	}
+	#spatial restrictions
+	if (length(v.spatial) > 0 ){
+	sp.rest_index <- !is.na(v.spatial)  # set those to NA which have a value in the restriction layer
+	v.alloc[sp.rest_index] <- v.lc [sp.rest_index]
+	}
+	
+	# Reclassify all natural to pseudo natural classes
+	if (length (natural) > 0 ){
+	ind <- is.element(v.alloc, natural)
+	v.alloc [ind] <- pseudo.N # natural vegetation to pseudo.N class (including areas of spatial restrictions)
+	
+	# Reclassify pseudo natural class based on trajectory and succession order
+	pseudo.index <- which(is.element(v.alloc, pseudo.N))
+		
+	if (length (pseudo.index) > 0){  
+		if(length (natural) > 1){
+			for (j in 1:length(pseudo.index)){
+            #can before.n be translated to natural 
+				for (a in length(natural):2){
+					if (traj[naturallookup[a], naturallookup[a-1]] < v.init.years[pseudo.index[j]] | v.lc[pseudo.index[j]] == natural[a-1]) {
+						v.alloc[pseudo.index[j]] <- natural[a-1]
+					} else{
+						v.alloc[pseudo.index[j]] <- natural[a]
+        } 
+      }
+    }
+    } 
+	if (length (natural)== 1) {
+		v.alloc[pseudo.index] <- natural
+	}
+	if (sum(is.element (v.alloc, pseudo.N))!=0) {
+		print( "Warning: error in natural vegetation module")
+		}
+    }}
+   
+	#####
+	#  3.4 Saving results and preparing next epoche
+	#####
+	#  	3.4.1 Updating transition years vector
+	#####
+	v.init.years <- ifelse(v.alloc==v.lc, v.init.years + 1, 1) #compare this allocation for transition years, inc if changed, reset to 1 if change
+	
+	v <- cbind(v.alloc, v.init.years)
+	
+	#### save to raster
+	# write to raster
+	out <- setValues(out, v)
+	pbStep(pb, i)
+	}
+
 	pbClose(pb)
 	return(out)
 	}
